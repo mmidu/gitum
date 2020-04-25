d79c6a9ed3a9df4257780093f13a91207e1def21
mmiduri-at-202617908855 <mmiduri@cutaway.it>
2020-04-25 16:29:48 +0200

    add import function

--- src/controllers/commandLine.go
+++ src/controllers/commandLine.go
@@ -16,25 +16,6 @@ import (
 // JSONStub is the default json structure of the git-credentials.json file
 const JSONStub = "{\n\t\"users\": [\n\t\t{\n\t\t\t\"identifier\": \"github-work\",\n\t\t\t\"credentials\": {\n\t\t\t\t\"username\": \"username\",\n\t\t\t\t\"password\": \"password1\",\n\t\t\t\t\"domain\": \"github.com\"\n\t\t\t}\n\t\t},\n\t\t{\n\t\t\t\"identifier\": \"two\",\n\t\t\t\"credentials\": {\n\t\t\t\t\"username\": \"uname2\",\n\t\t\t\t\"password\": \"password2\",\n\t\t\t\t\"domain\": \"domain\"\n\t\t\t}\n\t\t}\n\t]\n}"
 
-// Init initializes the JSON stub file
-func Init() {
-	path := fmt.Sprintf("%s/git-credentials.json", utils.GetHomeDir())
-	if !utils.FileExists(path) {
-		file, err := os.Create(path)
-		utils.Check(err)
-		defer file.Close()
-
-		w := bufio.NewWriter(file)
-		_, err = w.WriteString(JSONStub)
-		utils.Check(err)
-		w.Flush()
-	} else {
-		fmt.Println(fmt.Sprintf("%s/git-credentials.json file already exists.", utils.GetHomeDir()))
-	}
-
-	os.Exit(0)
-}
-
 // ManageArguments get arguments from the command line and executes the right functions
 func ManageArguments() string {
 	var identifier string
@@ -49,13 +30,27 @@ func ManageArguments() string {
 		case "-lu", "--listUsers":
 			printCredentials()
 		case "-c", "--current":
-			getCurrent()
+			current()
+		case "-ui", "--userImport":
+			importUser()
 		case "-h", "--help":
 			fmt.Println("HELP STRING")
 			os.Exit(0)
 		}
 	}
 	return identifier
+}
+
+// Init initializes the JSON stub file
+func Init() {
+	path := fmt.Sprintf("%s/git-credentials.json", utils.GetHomeDir())
+	if !utils.FileExists(path) {
+		writeInFile(path, JSONStub)
+	} else {
+		fmt.Println(fmt.Sprintf("%s/git-credentials.json file already exists.", utils.GetHomeDir()))
+	}
+
+	os.Exit(0)
 }
 
 func printCredentials() {
@@ -80,7 +75,7 @@ func getCredentials() models.Users {
 	return users
 }
 
-func getCurrent() {
+func getCurrentUser() map[string]string {
 	credentialsFile, err := os.Open(fmt.Sprintf("%s/.git-credentials", utils.GetHomeDir()))
 	utils.Check(err)
 	defer credentialsFile.Close()
@@ -101,14 +96,67 @@ func getCurrent() {
 
 	domain := strings.TrimSuffix(resB[1], "\n")
 
+	currentUser := map[string]string{
+		"username": username,
+		"password": password,
+		"domain":   domain,
+	}
+	return currentUser
+}
+
+func current() {
 	users := getCredentials()
+	currentUser := getCurrentUser()
 
 	for _, user := range users.Users {
-		if user.Credentials.Username == username && user.Credentials.Password == password && user.Credentials.Domain == domain {
+		if user.Credentials.Username == currentUser["username"] &&
+			user.Credentials.Password == currentUser["password"] &&
+			user.Credentials.Domain == currentUser["domain"] {
 			fmt.Println(user.Identifier)
 			os.Exit(0)
 		}
 	}
 
-	os.Exit(1)
+	os.Exit(0)
+}
+
+func importUser() {
+	users := getCredentials()
+	currentUser := getCurrentUser()
+
+	for _, user := range users.Users {
+		if user.Credentials.Username == currentUser["username"] &&
+			user.Credentials.Password == currentUser["password"] &&
+			user.Credentials.Domain == currentUser["domain"] {
+			os.Exit(0)
+		}
+	}
+
+	var newUser models.User
+
+	newUser.Identifier = currentUser["username"] + "@" + strings.SplitN(currentUser["domain"], ".", -1)[0]
+	newUser.Credentials.Username = currentUser["username"]
+	newUser.Credentials.Password = currentUser["password"]
+	newUser.Credentials.Domain = currentUser["domain"]
+
+	users.Users = append(users.Users, newUser)
+
+	data := map[string][]models.User{
+		"users": users.Users,
+	}
+
+	JSONData, _ := json.MarshalIndent(data, "", "    ")
+	writeInFile(fmt.Sprintf("%s/git-credentials.json", utils.GetHomeDir()), string(JSONData))
+	os.Exit(0)
+}
+
+func writeInFile(path string, content string) {
+	file, err := os.Create(path)
+	utils.Check(err)
+	defer file.Close()
+
+	w := bufio.NewWriter(file)
+	_, err = w.WriteString(content)
+	utils.Check(err)
+	w.Flush()
 }
